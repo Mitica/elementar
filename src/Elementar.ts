c801a0100742d57f0e2d3909e7b09a5adb4700f0
Dumitru Cantea <canteadumitru@gmail.com>
2017-12-29 11:20:11 +0200

    renamed elementar->Elementar

--- src/Elementar.ts
+++ src/Elementar.ts
@@ -4,7 +4,6 @@ import { ElementarOptions, ELEMENTAR_OPTIONS } from './options';
 import { cheerioFromHTML } from './utils';
 import { ElementsBuilder } from './ElementsBuilder';
 
-
 export function elementar(html: string, options?: ElementarOptions): IElement[] {
     if (typeof html !== 'string') {
         throw new Error(`'html' parameter is invalid`);
